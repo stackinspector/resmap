77f455eea605c2509810a6d58a5b6f93f8f3b523
stackinspector <stackinspector@126.com>
2023-08-18 03:09:43 +0800

    fix is_newest, adjust args, no postfix, exist hash

--- main.rs
+++ main.rs
@@ -18,13 +18,13 @@ fn assert_name_vaild(name: &[u8]) {
 }
 
 fn file_name_without_tail(name: &[u8]) -> &str {
-    let (name, tail) = name.split_at(name.len() - 4);
+    let (name, tail) = name.split_at(name.len() - EXT.len());
     assert_eq!(tail, EXT);
     std::str::from_utf8(name).unwrap()
 }
 
 fn file_name_insert_tail(name: &[u8], hash: &[u8]) -> String {
-    let (name, tail) = name.split_at(name.len() - 4);
+    let (name, tail) = name.split_at(name.len() - EXT.len());
     assert_eq!(tail, EXT);
     String::from_utf8([name, b"-", hash, EXT].concat()).unwrap()
 }
@@ -36,7 +36,12 @@ fn vaild_name(entry: &fs::DirEntry) -> String {
 }
 
 fn is_newest(name: &[u8]) -> bool {
-    name[0].is_ascii_lowercase()
+    for b in name {
+        if matches!(b, b'_') {
+            return false;
+        }
+    }
+    true
 }
 
 fn hash_file(file_path: &Path) -> String {
@@ -61,14 +66,13 @@ fn main() {
     let mut args = env::args_os();
     let _ = args.next();
     let path = PathBuf::from(args.next().unwrap());
+    let blobs_path = PathBuf::from(args.next().unwrap());
     let list_path = PathBuf::from(args.next().unwrap());
-    let mut list1_file_handle = fs::OpenOptions::new().create_new(true).write(true).open(list_path.join("mirror-cn")).unwrap();
-    let mut list2_file_handle = fs::OpenOptions::new().create_new(true).write(true).open(list_path.join("mirror-os")).unwrap();
-    let blobs_path = path.join(BLOB_PATH);
+    let mut list_file_handle = fs::OpenOptions::new().create_new(true).write(true).open(list_path).unwrap();
     for ty_entry in fs::read_dir(path).unwrap().map(Result::unwrap) {
         assert_dir(&ty_entry);
         let ty_name = vaild_name(&ty_entry);
-        if ty_name == BLOB_PATH || !is_newest(ty_name.as_bytes()) { continue; }
+        if !is_newest(ty_name.as_bytes()) { continue; }
         for file_entry in fs::read_dir(ty_entry.path()).unwrap().map(Result::unwrap) {
             assert_file(&file_entry);
             let file_name = vaild_name(&file_entry);
@@ -76,16 +80,20 @@ fn main() {
             let hash = hash_file(&file_path);
             let dst = if is_newest(file_name.as_bytes()) {
                 let dst_name = file_name_insert_tail(file_name.as_bytes(), hash.as_bytes());
-                writeln!(list1_file_handle, "{} d1.ldtstore.com.cn/{}/{}", file_name_without_tail(file_name.as_bytes()), BLOB_PATH, dst_name).unwrap();
-                writeln!(list2_file_handle, "{} d1.ldtstore.net/{}/{}", file_name_without_tail(file_name.as_bytes()), BLOB_PATH, dst_name).unwrap();
-                match fs::OpenOptions::new().create_new(true).write(true).open(blobs_path.join(&dst_name)) {
+                writeln!(list_file_handle, "{} d1.tool.pc.wiki/{}/{}", file_name_without_tail(file_name.as_bytes()), BLOB_PATH, dst_name).unwrap();
+                let dst_path = blobs_path.join(&dst_name);
+                match fs::OpenOptions::new().create_new(true).write(true).open(&dst_path) {
                     Ok(mut dst_file_handle) => {
                         let mut file_handle = fs::OpenOptions::new().read(true).open(&file_path).unwrap();
                         io::copy(&mut file_handle, &mut dst_file_handle).unwrap();
                         "created"
                     }
                     Err(err) => if err.kind() == io::ErrorKind::AlreadyExists {
-                        "exists "
+                        if hash_file(&dst_path) == hash {
+                            "exists "
+                        } else {
+                            panic!("exists file incorrect");
+                        }
                     } else {
                         panic!("called `Result::unwrap()` on an `Err` value: {:?}", err);
                     }
